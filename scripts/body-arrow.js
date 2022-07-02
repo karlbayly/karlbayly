b227d64bb837d0355d57acff970b877c11504892
Karl Bayly <karlbayly@gmail.com>
2022-07-02 22:52:47 +1200

    initial commit

---
+++ scripts/body-arrow.js
@@ -0,0 +1,24 @@
+//Gets button
+
+var mybutton = document.getElementById('body-arrow')
+
+// when scrolls past 100px from the top button apears
+
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = 'none'
+  } else {
+    mybutton.style.display = 'none'
+  }
+}
+
+// When the user clicks on the button, scroll to the top of page
+
+function topFunction() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
